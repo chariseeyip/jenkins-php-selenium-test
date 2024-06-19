c073b8b39db8e8e43e1648e7c286bc5240b023e5
chariseeyip <chariseeyip@gmail.com>
2024-06-19 05:38:57 -0400

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /home/kali/Desktop/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-sleep 1
+sleep 20
 set +x
 
 echo 'Now...'
