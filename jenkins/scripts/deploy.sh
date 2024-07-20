a0c1a33b05b2e2b94e1fac4b0bd024386ff10761
Charisee Yip <64692199+chariseeyip@users.noreply.github.com>
2024-07-20 14:44:23 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\chari\\Desktop\\SIT\\YEAR 2\\TRI 3\\ICT2216 SECURE SOFTWARE DEVELOPMENT\\TEST\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/chari/Desktop/SIT/YEAR_2/TRI_3/ICT2216_SECURE_SOFTWARE_DEVELOPMENT/TEST/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 20
 set +x
 
